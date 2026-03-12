1da1a64d5f85d90ca5f872b72320b2af5aa5492e
//G0G// <ktx06155@naver.com>
2026-03-12 16:11:31 +0900

    feat:add initial commit

---
+++ fisa-auth-platform/src/main/java/com/fisa/auth/platform/FisaOAuthApplication.java
@@ -0,0 +1,13 @@
+package com.fisa.auth.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FisaOAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FisaOAuthApplication.class, args);
+	}
+
+}
